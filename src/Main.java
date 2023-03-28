2925d34f7b7b03e90125388617e8a919a08bca45
Артем <artemgolyshkov@mail.ru>
2023-03-28 21:28:50 +0300

    Задача из билета № 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import jdk.jshell.Snippet;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(polindrom("Madam I'm adam"));
+    }
+    public static boolean polindrom(String text) {
+        String str1 = text.toUpperCase().replaceAll("[^A-Za-z]", "");
+        StringBuffer str2 = new StringBuffer(str1).reverse();
+        boolean prov = true;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                prov = false;
+                break;
+            }
+        }
+        return prov;
+
     }
 }
